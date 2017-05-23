9ba5cfc93ddecc70e53a7bb62c98e6efb09b379a
harukacode <harukacode@163.com>
2017-05-23 15:21:04 +0800

    second commit

--- src/main/java/com/harukacode/SZ/Demo1.java
+++ src/main/java/com/harukacode/SZ/Demo1.java
@@ -3,6 +3,7 @@ package com.harukacode.SZ;
 /**
  * Created by Administrator on 2017-5-23.
  */
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +11,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
 public class Demo1 {
     public static void main(String[] args) throws IOException {
 //获取配置文件的输入流
@@ -24,6 +27,11 @@ public class Demo1 {
         Course course = sqlSession.selectOne("mapper.findById",1);
 //输出此对象
         System.out.println(course);
+//执行 SQL 查询，得到一个对象
+        List<Course> list = sqlSession.selectList("mapper.findAll");
+        for(Course course1:list){
+            System.out.println(course1);
+        }
 //关闭会话
         sqlSession.close();
     }
